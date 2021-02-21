ce39796141b862b20c1854c79c1e219cb8be088b
Michał Bryjak <bryjak1133@gmail.com>
2021-02-22 00:57:15 +0100

    nothing important for this moment

---
+++ constants.py
@@ -0,0 +1,9 @@
+WORKING_FOLDER = 'working_folder'
+PNG_FOLDER = 'generated_images'
+
+all_folders = [
+    WORKING_FOLDER,
+    PNG_FOLDER
+]
+
+
---
+++ piece.py
@@ -0,0 +1,45 @@
+
+
+def get_8_char_s(s):
+    for c in [str(i) for i in range(10)]:
+        s = s.replace(c, '.' * int(c))
+    return s
+
+
+def get_fixed_s(s):
+    s += '!'
+    result = ''
+    cnt = 0 
+
+    for c in s:
+        if c == '.':
+            cnt += 1
+        else:
+            if cnt != 0:
+                result += str(cnt)
+                cnt = 0
+            result += c
+
+    print(result)
+    return result[:-1]
+
+
+class Piece:
+    def __init__(self, row, col, typ):
+        self.row = row # from 0 
+        self.col = col # from 0
+        self.typ = typ
+
+
+    def update_str(self, s):
+        splitted = s.split('/')
+        row_str = splitted[self.row]
+        row_str = get_8_char_s(row_str)
+        
+        l = list(row_str)
+        l[self.col] = self.typ
+        row_str = ''.join(l)
+
+        row_str = get_fixed_s(row_str)
+        l = splitted[:self.row] + [row_str] + splitted[self.row+1:]  
+        return '/'.join(l)
